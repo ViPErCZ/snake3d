c1fcc8f508fde96920903a8e6970ab979fb1848f
Martin Chudoba <martin.chudoba@seznam.cz>
2022-11-09 10:47:37 +0100

    [FTR] add unit tests

--- ItemsDto/Barriers.cpp
+++ ItemsDto/Barriers.cpp
@@ -1,5 +1,4 @@
 #include "Barriers.h"
-#include "ObjItem.h"
 
 namespace ItemsDto {
     Barriers::~Barriers() {
---
+++ Tests/main.cpp
@@ -0,0 +1,26 @@
+#include "../App.h"
+#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <cstdint>
+
+using namespace Handler;
+using namespace Manager;
+
+TEST_CASE( "Checking if field is empty to place food..." ) {
+
+    auto snake = new Snake;
+    snake->init();
+    snake->reset();
+    auto eat = new Eat;
+    auto barriers = new Barriers;
+    auto eatLocation = new EatLocationHandler(barriers, snake, eat);
+    auto levelManager = new LevelManager(1, 3, barriers);
+    levelManager->createLevel(2);
+
+    REQUIRE( barriers->getItems().size() == 40 );
+    REQUIRE( eatLocation->isFieldEmpty(4, 24) == false );
+    REQUIRE( eatLocation->isFieldEmpty(5, 24) == false );
+    REQUIRE( eatLocation->isFieldEmpty(4, 25) == true );
+    REQUIRE( eatLocation->isFieldEmpty(5, 25) == true );
+}
